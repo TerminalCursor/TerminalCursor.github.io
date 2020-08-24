79c345499d2fd9ac6623e1c1f2fa8ec00fb8e572
TerminalCursor <ctcunni1@asu.edu>
2020-08-23 19:10:03 -0700

    Updated Boostrapping script

--- SGRBS/pre-chroot.sh
+++ SGRBS/pre-chroot.sh
@@ -10,32 +10,30 @@ if [ "${PING_SUCCESS}" == "0" ]; then
 	read BLK_DEV
 	BLK_DEV=$(echo $BLK_DEV | awk '{print $1}')
 	echo Selected $BLK_DEV
-	printf "o\nn\np\n1\n\n+1M\nn\np\n2\n\n+16G\nn\np\n3\n\n\na\n1\np\nw\nq"
-	echo
-	printf "| fdisk /dev/${BLK_DEV}"
-	echo
-	printf "mkswap /dev/${BLK_DEV}2"
-	echo
-	printf "swapon /dev/${BLK_DEV}2"
-	echo
-	printf "mkfs.ext4 /dev/${BLK_DEV}3"
-	echo
-	printf "mount /dev/${BLK_DEV}3 /mnt/gentoo"
-	echo
+	printf "o\nn\np\n1\n\n+1M\nn\np\n2\n\n+16G\nn\np\n3\n\n\na\n1\np\nw\nq" | fdisk /dev/${BLK_DEV}
+	mkswap /dev/${BLK_DEV}2
+	swapon /dev/${BLK_DEV}2
+	mkfs.ext4 /dev/${BLK_DEV}3
+	mount /dev/${BLK_DEV}3 /mnt/gentoo
 	if [ "$?" == "0" ]; then
-		printf "cd /mnt/gentoo"
-		echo
+		cd /mnt/gentoo
 		AUTOBUILD_ISO=$(curl "https://gentoo.osuosl.org/releases/amd64/autobuilds/latest-stage3-amd64.txt" | grep "^[1-9]" | awk '{print $1}')
 		AUTOBUILD_DIR=$(printf "https://gentoo.osuosl.org/releases/amd64/autobuilds/${AUTOBUILD_ISO}")
+		curl -LO "$AUTOBUILD_DIR"
 		echo $AUTOBUILD_DIR
-		echo tar xpvf $(basename ${AUTOBUILD_ISO}) --xattrs-include='*.*' --numeric-owner
+		tar xpvf $(basename ${AUTOBUILD_ISO}) --xattrs-include='*.*' --numeric-owner
 		printf "What architecture? "
 		read ARCH
-		echo COMMON_FLAGS="-march=$ARCH -O2 -pipe"
-		echo CFLAGS="$${COMMON_FLAGS}"
-		echo CXXFLAGS="$${COMMON_FLAGS}"
+		printf "COMMON_FLAGS=\"-march=$ARCH -O2 -pipe\"\nCFLAGS=\"$${COMMON_FLAGS}\"\nCXXFLAGS=\"$${COMMON_FLAGS}\"" >> /mnt/gentoo/etc/portage.conf
 		NUM_CORE=$(lscpu | grep "^Core" | awk '{print $NF}')
-		echo MAKE_OPTS="-j$(($NUM_CORE + 1))"
+		printf "MAKE_OPTS="-j$(($NUM_CORE + 1))" >> /mnt/gentoo/etc/portage.conf
+		nano /mnt/gentoo/etc/portage.conf
+		cp --dereference /etc/resolv.conf /mnt/gentoo/etc
+		mount --types proc /proc /mnt/gentoo/proc
+		mount --rbind /sys /mnt/gentoo/sys
+		mount --make-rslave /mnt/gentoo/sys
+		mount --rbind /dev /mnt/gentoo/dev
+		mount --make-rslave /mnt/gentoo/dev
 	fi
 else
 	echo "NETWORK DISCONNECTED"
